b14307e659d2d276bdc6ffba62a31f2665857fc1
Peter Argall <pargall2@uwo.ca>
2014-08-12 22:16:14 -0400

    Successfully authed up

--- generateIcal.py
+++ generateIcal.py
@@ -1,2 +1,17 @@
 import requests
 import icalendar
+
+userName = "PARGALL2"
+password = "***"
+
+LoginURL = "https://student.uwo.ca/psp/heprdweb/EMPLOYEE/HRMS/c/UWO_WISG.WSA_STDNT_CENTER.GBL&languageCd=ENG";
+
+
+payload = {'httpPort2' : '',
+		'timezoneOffset2':0,
+		'userid':userName.upper(),
+		'pwd':password,
+		'Submit':'Sign In'}
+
+r = requests.post(LoginURL, params=payload);
+print(r.text)
