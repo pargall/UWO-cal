6ae55262b5de81841de03482113c96a69650955a
Peter Argall <pargall2@uwo.ca>
2014-08-12 23:24:39 -0400

    Grabbing the schedule from the server

--- generateIcal.py
+++ generateIcal.py
@@ -4,14 +4,20 @@ import icalendar
 userName = "PARGALL2"
 password = "***"
 
-LoginURL = "https://student.uwo.ca/psp/heprdweb/EMPLOYEE/HRMS/c/UWO_WISG.WSA_STDNT_CENTER.GBL&languageCd=ENG";
-
+LoginURL = 'https://student.uwo.ca/psp/heprdweb/EMPLOYEE/HRMS/c/UWO_WISG.WSA_STDNT_CENTER.GBL&languageCd=ENG';
+schedURL = 'https://student.uwo.ca/psc/heprdweb/EMPLOYEE/HRMS/c/SA_LEARNER_SERVICES.SSR_SSENRL_LIST.GBL';
 
 payload = {'httpPort2' : '',
 		'timezoneOffset2':0,
 		'userid':userName.upper(),
 		'pwd':password,
 		'Submit':'Sign In'}
+s = requests.Session();
 
-r = requests.post(LoginURL, params=payload);
+#login in
+s.post(LoginURL, params=payload);
+
+#get Sched
+r = s.post(schedURL, {'Page':'SSR_SSENRL_LIST'})
+
 print(r.text)
