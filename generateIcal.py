3f19d177e16f9c199b6a4c0f9ca030d1ebf226f5
Peter Argall <pargall2@uwo.ca>
2014-08-14 03:11:45 -0400

    Now grabbing the data from the table. Now to do put it into a class

--- generateIcal.py
+++ generateIcal.py
@@ -1,8 +1,24 @@
 import requests
 import icalendar
+from datetime import date
+from lxml import html
 
+class Course:
+	name = ""
+	sections = []
+
+	def __init__(self, n):
+		name = n
+
+class Section:
+	name =""
+	times = []
+	startDate = date.today()
+	endDate = date.today()
+
+courseList = []
 userName = "PARGALL2"
-password = "***"
+password = '***'
 
 LoginURL = 'https://student.uwo.ca/psp/heprdweb/EMPLOYEE/HRMS/c/UWO_WISG.WSA_STDNT_CENTER.GBL&languageCd=ENG';
 schedURL = 'https://student.uwo.ca/psc/heprdweb/EMPLOYEE/HRMS/c/SA_LEARNER_SERVICES.SSR_SSENRL_LIST.GBL';
@@ -20,4 +36,30 @@ s.post(LoginURL, params=payload);
 #get Sched
 r = s.post(schedURL, {'Page':'SSR_SSENRL_LIST'})
 
-print(r.text)
+
+tree = html.fromstring(r.text)
+courses = tree.xpath("//div[starts-with(@id,'win0divDERIVED_REGFRM1_DESCR20$')]")
+
+#Parse the raw HTML into objects
+for course in courses:
+	#Get the course title
+	name = course.xpath("descendant::td[@class = 'PAGROUPDIVIDER']")
+	c = Course(name[0].text) 
+
+	#Find the sections we're signed uo for
+	sections =  course.xpath("descendant::tr[starts-with(@id,'trCLASS_MTG_VW$')]")
+
+	#print sections
+
+	for s in sections:
+		col = s.xpath("descendant::td/div/span")
+		for c in col:
+			try:
+				print c.text
+			except Exception, e:
+				pass			
+	#print name[0].text
+
+#print courses
+#parser = ScheduleParser()
+#parser.feed(r.text)
